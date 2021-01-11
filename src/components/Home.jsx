097ac56fe604343387846e0685d8a3b7f1383548
al-abbas-nz <al.mus930@gmail.com>
2021-01-11 18:25:52 +1300

    add nav bar

---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <main>
+      <img src="" alt="" />
+      <section>
+        <h1>Hello</h1>
+      </section>
+    </main>
+  );
+}
