7b4280a1188df948e36f48c7efee20cb751e96a8
al-abbas-nz <al.mus930@gmail.com>
2021-01-12 20:15:53 +1300

    sanity cms and GROQ working

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,12 @@
-import React from "react";
+import React from 'react';
+import image from '../images/tak.jpg'
 
 export default function Home() {
   return (
     <main>
-      <img src="" alt="" />
-      <section>
-        <h1>Hello</h1>
+      <img src={image} alt='Background image of Takehe' className='absolute object-cover w-full h-full' />
+      <section className='relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8'>
+        <h1 className='text-6xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name'>Hello</h1>
       </section>
     </main>
   );
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,55 +1,55 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
-import { SocialIcon } from "react-social-icons";
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { SocialIcon } from 'react-social-icons';
 
 export default function NavBar() {
   return (
-    <header className="bg-red-600">
-      <div className="container mx-auto flex justify-between">
-        <nav className="flex">
+    <header className='bg-red-600'>
+      <div className='container mx-auto flex justify-between'>
+        <nav className='flex'>
           <NavLink
             exact
-            to="/"
-            activeClassName="text-white"
-            className="inflex-flex items-center py-6 px-3 mr-4 text-red-100 hover:text-green-800 text-4xl font-bold cursive tracking-widest"
+            to='/'
+            activeClassName='text-white'
+            className='inflex-flex items-center py-6 px-3 mr-4 text-red-100 hover:text-green-800 text-4xl font-bold cursive tracking-widest'
           >
             Al
           </NavLink>
           <NavLink
-            to="/post"
-            activeClassName="text-red-100 bg-red-700"
-            className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800"
+            to='/post'
+            activeClassName='text-red-100 bg-red-700'
+            className='inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800'
           >
             Blog Posts
           </NavLink>
           <NavLink
-            to="/project"
-            activeClassName="text-red-100 bg-red-700"
-            className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800"
+            to='/project'
+            activeClassName='text-red-100 bg-red-700'
+            className='inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800'
           >
             Projects
           </NavLink>
           <NavLink
-            to="/about"
-            activeClassName="text-red-100 bg-red-700"
-            className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800"
+            to='/about'
+            activeClassName='text-red-100 bg-red-700'
+            className='inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800'
           >
             About Me
           </NavLink>
         </nav>
-        <div className="inline-flex py-3 px-3 my-6">
+        <div className='inline-flex py-3 px-3 my-6'>
           <SocialIcon
-            url="https://www.linkedin.com/in/al-abbas-14401b1b8/"
-            className="mr-4"
-            target="_blank"
-            fgColor="#fff"
+            url='https://www.linkedin.com/in/al-abbas-14401b1b8/'
+            className='mr-4'
+            target='_blank'
+            fgColor='#fff'
             style={{ height: 35, width: 35 }}
           />
           <SocialIcon
-            url="https://github.com/al-abbas-nz"
-            className="mr-4"
-            target="_blank"
-            fgColor="#fff"
+            url='https://github.com/al-abbas-nz'
+            className='mr-4'
+            target='_blank'
+            fgColor='#fff'
             style={{ height: 35, width: 35 }}
           />
         </div>
