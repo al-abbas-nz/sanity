f0ec701a54e9fa1e2aee7129030b3ca9cd64f351
al-abbas-nz <al.mus930@gmail.com>
2021-01-13 21:42:23 +1300

    change image name to takehe

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import image from '../images/tak.jpg'
+import takehe from '../images/tak.jpg'
 
 export default function Home() {
   return (
     <main>
-      <img src={image} alt='Background image of Takehe' className='absolute object-cover w-full h-full' />
+      <img src={takehe} alt='Background image of Takehe' className='absolute object-cover w-full h-full' />
       <section className='relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8'>
         <h1 className='text-9xl text-green-100 font-bold cursive leading-none lg:leading-snug'>Hello</h1>
       </section>
