c605afb5ec1fc9883f576ce80fe5206453db0f77
al-abbas-nz <al.mus930@gmail.com>
2021-01-13 21:47:50 +1300

    change takehe image alt

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,7 +4,7 @@ import takehe from '../images/tak.jpg'
 export default function Home() {
   return (
     <main>
-      <img src={takehe} alt='Background image of Takehe' className='absolute object-cover w-full h-full' />
+      <img src={takehe} alt='Takehe' className='absolute object-cover w-full h-full' />
       <section className='relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8'>
         <h1 className='text-9xl text-green-100 font-bold cursive leading-none lg:leading-snug'>Hello</h1>
       </section>
